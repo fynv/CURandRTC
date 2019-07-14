0bd41fadaf140c7eb05ba984754171d846083060
feiy <feiy@nvidia.com>
2019-07-14 17:49:53 +0800

    Setting version number to 0.0.5: failed to fix a bug in linux case.

--- python/setup.py
+++ python/setup.py
@@ -50,7 +50,7 @@ module_CURandRTC = Extension(
 
 setup(
 	name = 'CURandRTC',
-	version = '0.0.4',
+	version = '0.0.5',
 	description = 'Random number generation module based on ThrustRTC',
 	long_description=long_description,
 	long_description_content_type='text/markdown',  
